e68ddb13585bab4f77dd7ff996147778617f3271
pochi <gddev2001@gmail.com>
2026-01-21 17:19:24 +0800

    updated ProductSlider UI
    - resized thumbnail size
    - added ratings
    - resized price label
    - break-all text title

--- components/ProductsSlider/ProductsSlider.jsx
+++ components/ProductsSlider/ProductsSlider.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import Image from 'next/image'
+import { StarIcon, StarHalfIcon } from 'lucide-react'
 
 export default function ProductsSlider({ items }) {
     // console.log(items)
@@ -8,13 +9,17 @@ export default function ProductsSlider({ items }) {
             { items.map((item, index) => {
                 return (
                     <Link key={index} href={`/product?id=${item['id']}`} className="flex flex-col py-2 rounded-[10px] bg-white px-4 mx-1 border border-[#ccc] hover:underline">
-                        <div className="relative w-35 h-30 p-2 object-contain">
+                        <div className="relative w-30 h-25 p-2 object-contain">
                             <Image alt="Thumbnail" src={item['thumbnail']} fill></Image>
                         </div>
-                        <div className="max-h-15">{item['title']}</div>
+                        <div className="max-h-15 leading-none line-clamp-2 break-all mb-1">{item['title']}</div>
                         <div className="flex flex-row items-end mt-auto">
                             <div className="text-[14px]">$</div>
-                            <div className="text-[18px]">{item['price']}</div>
+                            <div className="text-[16px]">{item['price']}</div>
+                            <div className="flex flex-row ml-auto items-center">
+                                { item['rating'] > 3.0 ? <StarIcon fill="orange" stroke="0" size={18}></StarIcon> : <StarHalfIcon fill="orange" stroke="0" color="orange" size={18}></StarHalfIcon> }
+                                <div>{item['rating']}</div>
+                            </div>
                         </div>
                     </Link>
                 )
