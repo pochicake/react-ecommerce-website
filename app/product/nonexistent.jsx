d1a8830435ee34fefe6d5baf24e7796cd2519926
pochi <gddev2001@gmail.com>
2026-01-30 10:14:46 +0800

    Update /product/nonexistent.jsx
    - Use BaseLayout
    - Proper message
    - Proper layout

--- app/product/nonexistent.jsx
+++ app/product/nonexistent.jsx
@@ -1,17 +1,19 @@
 // Used to display a page when a product does not exist, or the URL provided is invalid
-"use server"
-
-import Header from "@/components/Header/Header";
+import BaseLayout from "@/components/BaseLayout";
+import { Frown } from "lucide-react";
 import Link from "next/link";
 
 export default async function ErrorNonExistent() {
     return (
-        <div className="flex flex-col h-full w-full">
-        <Header></Header>
-        <div className="m-auto">
-            <div>Sorry! The product does not exist or the link provided is invalid!</div>
-            <Link href="/">Go to homepage.</Link>
-        </div>
-        </div>
+        <BaseLayout>
+            <div className="flex flex-row gap-2 m-auto rounded-[10px] border-[#aaa] border bg-white p-4">
+                <Frown size={60} color="#555" className="my-auto"/>
+                <div className="block">
+                    <div className="text-[20px] font-bold">Product not found</div>
+                    <div>Oh no! It looks like the product does not exist or the link provided is invalid!</div>
+                    <Link href="/" className="font-bold hover:underline">Go to homepage.</Link>
+                </div>
+            </div>
+        </BaseLayout>
     )
 }
