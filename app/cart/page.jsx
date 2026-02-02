cc9cc6aa0b25df7ae6168f838d40035098162f36
pochi <gddev2001@gmail.com>
2026-02-02 11:21:27 +0800

    Update app/cart/page.jsx for BaseLayout

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,14 +1,9 @@
-import Footer from "@/components/Footer/Footer";
-import Header from "@/components/Header/Header";
+import BaseLayout from "@/components/BaseLayout";
 
-export default function CartPage() {
+export default async function CartPage() {
     return (
-        <>
-            <Header></Header>
-            <div>
-                
-            </div>
-            <Footer></Footer>
-        </>
+        <BaseLayout>
+        
+        </BaseLayout>
     )
 }
